f87a60678aedc4b24b498c5d5d62ce78bf61fa8b
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-13 11:36:00 +0800

    feat: install handlebars

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use('',(req,res)=>{
-  res.send('hello world')
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/',(req,res)=>{
+  res.render('index')
 })
 app.listen(port,(req,res)=>{
   console.log(`this app listen to http://localhost:${port}`)
