497075181686e28bf61a3edb51c508c6c2c25c05
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-18 15:00:49 +0800

    feat: init blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,15 +1,32 @@
+const db = require('../models')
+const Blog = db.Blog
+
 const blogController = {
   getBlog:(req, res, next) =>{
-    res.send('blog')
+    res.render('index')
   },
   createBlog:(req,res,next) =>{
+    const {description} = req.body
+
+    Blog.create({description:description}).then(() =>{
+      return res.json('all right')
+    }).catch(next)
 
   },
   deleteBlog:(req,res,next) =>{
-
+    const {blogId} = req.params
+    
+    Blog.findByPk(blogId).then(blog=>{
+      return blog.destroy()
+    }).catch(next)
   },
   updateBlog:(req,res,next)=>{
+    const {blogId} = req.params
+    const {description} = req.body
     
+    Blog.findByPk(blogId).then(blog=>{
+      return blog.update({description:description})
+    }).catch(next)
   }
 }
 
