69324fba7f2200ef7050bb97179ed30024045fa3
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-20 17:47:52 +0800

    feat: add dayjs and order by date

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -1,12 +1,15 @@
 const db = require('../models')
 const Tracker = db.Tracker
+const dayjs = require('dayjs')
 
 const trackController = {
   getRecord:(req, res, next)=>{
-    Tracker.findAll({raw: true}).then(tracker=>{
-      res.render('tracker')
+    Tracker.findAll({raw: true,order:[['date','ASC']]}).then(records=>{
+      records.forEach(record=>{{
+        record.date = dayjs(record.date).format('YYYY/MM/DD')
+      }})
+      return res.render('tracker',{records})
     }).catch(next)
-    
   },
   getCreateRecord:(req, res, next)=>{
     res.render('newRecord')
