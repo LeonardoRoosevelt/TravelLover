027a00ef6530c71815883a7de7a7f4d2ef9a8550
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-21 16:05:23 +0800

    feat: add font-awesome

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const db = require('./models')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
-
 
 app.listen(port,(req,res)=>{
   console.log(`this app listen to http://localhost:${port}`)
