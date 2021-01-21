a4ecf12a67d5407e677f5639c797bcf0c5c2cf47
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-21 15:42:08 +0800

    fix: associate between category and tracker

--- models/tracker.js
+++ models/tracker.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Tracker.hasMany(models.Category)
+      Tracker.belongsTo(models.Category)
     }
   };
   Tracker.init({
