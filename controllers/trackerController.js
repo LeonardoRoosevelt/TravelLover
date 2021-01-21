1c87f17043671029aab1080b412db78c3f692f4e
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-21 16:04:38 +0800

    refactor: include category function

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -1,26 +1,32 @@
 const db = require('../models')
 const Tracker = db.Tracker
+const Category = db.Category
 const dayjs = require('dayjs')
 
 const trackController = {
   getRecord:(req, res, next)=>{
     let totalAmount = 0
-    Tracker.findAll({raw: true,order:[['date','ASC']]}).then(records=>{
-      records.forEach(record=>{{
+    Tracker.findAll({raw:true,nest:true,order:[['date','ASC']],include:[{model:Category}]}).then(records=>{
+      records.forEach(record=>{
         record.date = dayjs(record.date).format('YYYY/MM/DD')
         totalAmount += record.price
-      }})
+        record.CategoryId = record.Category.icon
+      })
       return res.render('tracker',{records, totalAmount})
     }).catch(next)
   },
   getCreateRecord:(req, res, next)=>{
-    res.render('newRecord')
+    Category.findAll({raw:true}).then(categories=>{
+      res.render('newRecord',{categories})
+    })
+    
   },
   createRecord:(req, res, next)=>{
-    const {product,date,price} = req.body
-    Tracker.create({product, date, price}).then(record => {
+    const {product,date,price,categoryId} = req.body
+      Tracker.create({product, date, price,CategoryId:categoryId}).then(record => {
       res.redirect('/trackers')
-    }).catch(next)
+    })
+    .catch(next)
   },
   deleteRecord:(req, res, next)=>{
 
