d9308b6ccc31a97b98bb16db67be97e90319d631
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-27 16:27:14 +0800

    feat: add marker clusterer

--- public/javascript/mapFunction.js
+++ public/javascript/mapFunction.js
@@ -38,6 +38,11 @@ function initMap() {
     )
     infoWindow.open(map)
   })
+
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+  })
 }
 
 const locations = [
