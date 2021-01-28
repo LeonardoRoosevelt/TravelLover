aa0719b6ef7dd2a1b20cc45a9d01da5be44a45d8
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-28 22:02:41 +0800

    feat: add map.json route

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -1,8 +1,18 @@
+const db = require('../models')
+const Blog = db.Blog
+const Marker = db.Marker
+
 const mapController = {
   getMap: (req, res, next) => {
     res.render('map')
   },
-  getMapJson: (req, res, next) => {},
+  getMapJson: (req, res, next) => {
+    Marker.findAll({
+      attributes: ['lat', 'lng']
+    }).then((markers) => {
+      return res.json(markers)
+    })
+  }
 }
 
 module.exports = mapController
--- routes/modules/map.js
+++ routes/modules/map.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const mapController = require('../../controllers/mapController')
 
 router.get('/', mapController.getMap)
+router.get('/json', mapController.getMapJson)
 
 module.exports = router
