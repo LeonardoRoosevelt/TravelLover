a28abd968d0a08707d3e15e316e165a4e07cace9
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-01-29 16:03:12 +0800

    refactor: modify mapFunction more completely

--- public/javascript/mapFunction.js
+++ public/javascript/mapFunction.js
@@ -1,30 +1,43 @@
 let markers = []
 function initMap() {
-  let infoWindow = new google.maps.InfoWindow()
   const map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 25.047817, lng: 121.516959 },
-    zoom: 8
+    zoom: 13
   })
-
-  addMarkers(map)
+  let infoWindow = new google.maps.InfoWindow()
+  setMarkers(map)
 
   // click listener configures by PlaceId.
   map.addListener('click', (mapsMouseEvent) => {
     // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng
-    })
     if (mapsMouseEvent.placeId) {
+      mapsMouseEvent.stop()
       let request = { placeId: mapsMouseEvent.placeId, fields: ['name', 'geometry'] }
       let service = new google.maps.places.PlacesService(map)
       service.getDetails(request, (place, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-          infoWindow.close()
-          infoWindow.setContent(
-            `<div></div><span id="place-name" class="title" style="text-align: center; display:block;">${place.name}</span><br/>
+          let infoContent = `<div></div><span id="place-name" class="title" style="text-align: center; display:block;">${place.name}</span><br/>
            <a class="btn btn-primary" href="/blogs/createBlog/${place.geometry.location}/${place.name}">Create Blog</a><a class="btn btn-success ml-3" href="/trackers/createRecord/${place.geometry.location}/${place.name}">Create Tracker</a></div>`
-          )
-          infoWindow.open(map)
+          DeleteMarker(1)
+          marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            id: 1,
+            icon: { url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' }
+          })
+          markers.push(marker)
+          infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng
+          })
+          infoWindow.close()
+          infoWindow.setContent(infoContent)
+          infoWindow.open(map, marker)
+          marker.addListener('click', () => {
+            infoWindow.close()
+            infoWindow.setContent(infoContent)
+            infoWindow.open(map, marker)
+          })
+          map.setCenter(place.geometry.location)
         }
       })
     }
@@ -37,39 +50,42 @@ function initMap() {
   // Specify just the place data fields that you need.
   autocomplete.setFields(['place_id', 'geometry', 'name'])
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input)
-  const infowindowContent = document.getElementById('infowindow-content')
-  infoWindow.setContent(infowindowContent)
+
   autocomplete.addListener('place_changed', () => {
-    infoWindow.close()
     const place = autocomplete.getPlace()
+    let infoContent = `<span id="place-name" class="title" style="text-align: center; display:block;">${place.name}</span><br/>
+        <a class="btn btn-primary" href="/blogs/createBlog/${place.geometry.location}/${place.name}">Create Blog</a><a class="btn btn-success ml-3" href="/trackers/createRecord/${place.geometry.location}/${place.name}">Create Tracker</a>`
     if (!place.geometry) {
       return
     }
-    const placeMarker = new google.maps.Marker({ map: map })
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport)
     } else {
       map.setCenter(place.geometry.location)
-      map.setZoom(17)
+      map.setZoom(13)
     }
-    // Set the position of the marker using the place ID and location.
-    placeMarker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location
+    DeleteMarker(1)
+    marker = new google.maps.Marker({
+      position: place.geometry.location,
+      map: map,
+      id: 1,
+      icon: { url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' }
     })
-
-    placeMarker.addListener('click', (mapsMouseEvent) => {
-      infoWindow.close(map)
-      infoWindow = new google.maps.InfoWindow({
-        position: place.geometry.location
-      })
-      infoWindow.setContent(
-        `<span id="place-name" class="title" style="text-align: center; display:block;">${place.name}</span><br/>
-        <a class="btn btn-primary" href="/blogs/createBlog/${mapsMouseEvent.latLng}/${place.name}">Create Blog</a><a class="btn btn-success ml-3" href="/trackers/createRecord/${mapsMouseEvent.latLng}/${place.name}">Create Tracker</a>`
-      )
-      infoWindow.open(map)
+    markers.push(marker)
+    infoWindow = new google.maps.InfoWindow({
+      position: place.geometry.location
     })
+    infoWindow.close()
+    infoWindow.setContent(infoContent)
+    infoWindow.open(map, marker)
+    marker.addListener('click', () => {
+      infoWindow.close()
+      infoWindow.setContent(infoContent)
+      infoWindow.open(map, marker)
+    })
+    map.setCenter(place.geometry.location)
   })
+
   // geolocation
   const locationButton = document.getElementById('geoBtn')
   locationButton.textContent = 'Get Current Location'
@@ -84,14 +100,29 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           }
-          infoWindow.setPosition(pos)
-          infoWindow.setContent(
-            `<span id="place-name" class="title" style="text-align: center; display:block;">本地定位</span><br/>
+          DeleteMarker(1)
+          let infoContent = `<span id="place-name" class="title" style="text-align: center; display:block;">本地定位</span><br/>
             <a class="btn btn-primary" href="/blogs/createBlog/(${pos.lat}, ${pos.lng})">Create Blog</a><a class="btn btn-success ml-3" href="/trackers/createRecord/(${pos.lat}, ${pos.lng})">Create Tracker</a>`
-          )
-          infoWindow.open(map)
+          marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            id: 1,
+            icon: { url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' }
+          })
+          markers.push(marker)
+          infoWindow = new google.maps.InfoWindow({
+            position: pos
+          })
+          infoWindow.close()
+          infoWindow.setContent(infoContent)
+          infoWindow.open(map, marker)
+          marker.addListener('click', () => {
+            infoWindow.close()
+            infoWindow.setContent(infoContent)
+            infoWindow.open(map, marker)
+          })
           map.setCenter(pos)
-          map.setZoom(17)
+          map.setZoom(13)
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter())
@@ -114,7 +145,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map)
 }
 
-function addMarkers(map) {
+function setMarkers(map) {
   let infoWindow = new google.maps.InfoWindow()
   axios
     .get('/map/json')
@@ -126,17 +157,17 @@ function addMarkers(map) {
           map: map
         })
         marker.addListener('click', (mapsMouseEvent) => {
-          // Close the other InfoWindow.
-          infoWindow.close()
-          // Create a new InfoWindow.
           infoWindow = new google.maps.InfoWindow({
             position: mapsMouseEvent.latLng
           })
+          infoWindow.close()
           infoWindow.setContent(
             `<span id="place-name" class="title" style="text-align: center; display:block;">${l.Blogs[0].location}</span><br/>
           <a class="btn btn-primary" href="/blogs/createBlog/${mapsMouseEvent.latLng}/${l.Blogs[0].location}">Create Blog</a><a class="btn btn-success ml-3" href="/trackers/createRecord/${mapsMouseEvent.latLng}/${l.Blogs[0].location}">Create Tracker</a>`
           )
           infoWindow.open(map)
+          map.setCenter(mapsMouseEvent.latLng)
+          map.setZoom(13)
         })
         markers.push(marker)
       })
@@ -150,4 +181,33 @@ function addMarkers(map) {
     .catch(function (error) {
       console.log(error)
     })
+}
+
+function DeleteMarker(id) {
+  //Find and remove the marker from the Array
+  for (var i = 0; i < markers.length; i++) {
+    if (markers[i].id == id) {
+      //Remove the marker from Map
+      markers[i].setMap(null)
+      //Remove the marker from array.
+      markers.splice(i, 1)
+      return
+    }
+  }
+}
+
+function fixInfoWindow() {
+  //Here we redefine set() method.
+  //If it is called for map option, we hide InfoWindow, if "noSupress" option isnt true.
+  //As Google doesn't know about this option, its InfoWindows will not be opened.
+  var set = google.maps.InfoWindow.prototype.set
+  google.maps.InfoWindow.prototype.set = function (key, val) {
+    if (key === 'map') {
+      if (!this.get('noSupress')) {
+        console.log('This InfoWindow is supressed. To enable it, set "noSupress" option to true')
+        return
+      }
+    }
+    set.apply(this, arguments)
+  }
 }
