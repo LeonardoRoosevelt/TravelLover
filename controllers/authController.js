16ae774bb471cfe14ff75ce2ae8789b3f97cd622
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-02-04 13:11:38 +0800

    feat: add signin page and signup function

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,14 +7,36 @@ const authController = {
     return res.render('signup')
   },
 
-  signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then((user) => {
-      return res.redirect('/signin')
+  signUp: (req, res, next) => {
+    const { name, account, email, password } = req.body
+    User.findOrCreate({
+      where: {
+        email: email,
+        account: account
+      },
+      defaults: {
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      }
     })
+      .then(([user, created]) => {
+        return res.redirect('/signin')
+      })
+      .catch(next)
+  },
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+
+  signIn: (req, res) => {
+    res.redirect('/blogs')
+  },
+
+  logout: (req, res) => {
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = authController
