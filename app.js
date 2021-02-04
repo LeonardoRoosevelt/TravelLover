ef9ad16ef6999dca25d8e0b1366c799c2cdc6ae9
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-02-04 11:37:18 +0800

    feat: install express-session connect-flash

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
+
+const app = express()
+const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(routes)
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
   console.log(`this app listen to http://localhost:${port}`)
 })
