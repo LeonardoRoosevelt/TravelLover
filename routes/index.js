15cdd469b6ab25d061da8e9ea90930cf8051bf91
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-02-04 16:44:57 +0800

    feat: add authenticate function

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,12 @@ const user = require('./modules/users')
 const tracker = require('./modules/trackers')
 const map = require('./modules/map')
 const auth = require('./modules/auth')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/users', user)
-router.use('/blogs', blog)
-router.use('/trackers', tracker)
-router.use('/map', map)
+router.use('/blogs', authenticator, blog)
+router.use('/trackers', authenticator, tracker)
+router.use('/map', authenticator, map)
+router.use('/users', authenticator, user)
 router.use('/', auth)
 
 module.exports = router
