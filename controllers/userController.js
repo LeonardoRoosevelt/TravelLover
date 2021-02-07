5ac7097e9af58ee33ff8b2aa18a18815272d7029
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-02-07 13:01:19 +0800

    feat: install imgur & mutler for updateUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models')
 const User = db.User
 
@@ -12,7 +14,60 @@ const userController = {
       })
       .catch(next)
   },
-  getEditUser: (req, res, next) => {},
-  updateUser: (req, res, next) => {}
+  getEditUser: (req, res, next) => {
+    User.findByPk(req.params.id).then((user) => {
+      return res.render('editProfile', { user: req.user, profile: user.toJSON() })
+    })
+  },
+  updateUser: (req, res, next) => {
+    const UserId = req.params.id
+    const { name } = req.body
+    if (!name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    const files = req.files || {}
+    console.log(files)
+    if (files) {
+      imgur.setClientId(IMGUR_CLIENT_ID)
+      if (files.avatar && files.background) {
+        const avatarFile = files.avatar[0].path
+        const backgroundFile = files.background[0].path
+        return Promise.all([imgur.uploadFile(avatarFile), imgur.uploadFile(backgroundFile)])
+          .then(([avatar, background]) => updateProfile(req, res, UserId, name, avatar.data.link, background.data.link))
+          .catch(next)
+      } else if (files.avatar) {
+        const avatarFile = files.avatar[0].path
+
+        return imgur
+          .uploadFile(avatarFile)
+          .then((avatar) => updateProfile(req, res, UserId, name, avatar.data.link, null))
+          .catch(next)
+      } else if (files.background) {
+        const backgroundFile = files.background[0].path
+        return imgur
+          .uploadFile(backgroundFile)
+          .then((background) => updateProfile(req, res, UserId, name, null, background.data.link))
+          .catch(next)
+      } else {
+        return Promise.all([updateProfile(req, res, UserId, name, null, null)]).catch(next)
+      }
+    }
+  }
+}
+
+function updateProfile(req, res, UserId, name, newAvatar, newBackground) {
+  User.findByPk(UserId).then((user) => {
+    user
+      .update({
+        name: name,
+        avatar: newAvatar ? newAvatar : user.avatar,
+        background: newBackground ? newBackground : user.background
+      })
+      .then((user) => {
+        req.flash('success_messages', 'user was successfully to update')
+        res.redirect(`/users/${user.id}`)
+      })
+  })
 }
 module.exports = userController
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/userController')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 router.get('/:id', userController.getUser)
 router.get('/:id/edit', userController.getEditUser)
-router.put('/:id', userController.updateUser)
+router.put(
+  '/:id',
+  upload.fields([
+    { name: 'avatar', maxCount: 1 },
+    { name: 'background', maxCount: 1 }
+  ]),
+  userController.updateUser
+)
 
 module.exports = router
