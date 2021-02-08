96ebc940d1a333223ecd1debf627ea268bfbebfc
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2021-02-08 19:35:50 +0800

    fix: signin message

--- config/passport.js
+++ config/passport.js
@@ -19,7 +19,8 @@ passport.use(
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
         if (!bcrypt.compareSync(password, user.password))
           return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-        return cb(null, user)
+
+        return cb(null, user, req.flash('success_messages', '成功登入！'))
       })
     }
   )
--- controllers/authController.js
+++ controllers/authController.js
@@ -42,12 +42,6 @@ const authController = {
   signInPage: (req, res) => {
     return res.render('signin')
   },
-
-  signIn: (req, res) => {
-    req.flash('success_messages', '成功登入！')
-    res.redirect('/blogs')
-  },
-
   logout: (req, res) => {
     req.flash('success_messages', '登出成功！')
     req.logout()
